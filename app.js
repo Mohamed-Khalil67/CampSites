4d7982f279c737a0d08667194eebb3e1b4001aa1
Mohamed Khalil <mohamed.khalil.ucbl@gmail.com>
2025-08-02 14:12:06 +0300

    style updated , ejs layout, footer + navbar

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
-const path = require('path');
 const express = require('express');
-const Campground = require('./models/campground');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -14,6 +15,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
