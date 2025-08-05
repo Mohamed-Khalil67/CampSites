2f1f6da28be589c690fb39581cf70bdd83ad29d7
Mohamed Khalil <mohamed.khalil.ucbl@gmail.com>
2025-08-05 16:54:04 +0300

    Error catching using custom function + client also

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -35,17 +37,26 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
-  const campground = new Campground(req.body.campground); // json body sent by form post method with name=campground[title]
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post(
+  '/campgrounds',
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400);
+    const campground = new Campground(req.body.campground);
+    // json body sent by form post method with name=campground[title]
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
-app.get('/campgrounds/:id', async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id);
-  res.render('campgrounds/show', { campground });
-});
+app.get(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+  })
+);
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
   const { id } = req.params;
@@ -67,6 +78,16 @@ app.delete('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+});
+
+app.all(/(.*)/, (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode).send(message);
+  res.send('Oh Boy , here we go again!');
 });
 
 app.listen(3000, () => {
